2cee23c7c826a55972aeecd17904aea292fde32e
rshezarr <rshezarr@gmail.ru>
2022-12-15 15:12:13 +0600

    add: commentary

--- main.go
+++ main.go
@@ -25,18 +25,21 @@ func main() {
 }
 
 func dijkstraAlgorithm(table [][]int, start int, dest int) ([]int, error) {
-	var index int = start
 	var value int
-	// var sum int
 	var path []int
 	var err error
+	// Start at the starting node
+	index := start
 
+	// Loop until we reach the destination node
 	for index != dest {
+		// Find the next node with the minimum weight
 		index, value, err = findMinWeight(table[index], value)
 		if err != nil {
 			return nil, err
 		}
-		// sum += value
+
+		// Add the weight to the path
 		path = append(path, value)
 	}
 
@@ -46,6 +49,8 @@ func dijkstraAlgorithm(table [][]int, start int, dest int) ([]int, error) {
 func findMinWeight(node []int, prev int) (int, int, error) {
 	var index int
 	var value int
+
+	// Find the first non-zero, non-prev value in the node
 	for i, v := range node {
 		if v != prev && v != 0 {
 			value = v
@@ -54,18 +59,24 @@ func findMinWeight(node []int, prev int) (int, int, error) {
 		}
 	}
 
+	// Find the minimum value that is not prev or 0
 	for i := index; i < len(node); i++ {
+		// Check for negative values
 		if node[i] < 0 {
 			return 0, 0, errors.New("found negative value")
 		}
 
+		// Skip prev and 0 values
 		if node[i] == prev || node[i] == 0 {
 			continue
 		}
+
+		// Update the minimum value and index
 		if node[i] < value {
 			value = node[i]
 			index = i
 		}
 	}
+
 	return index, value, nil
 }
